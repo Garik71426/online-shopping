b4dbccc63d2bfb78edfae3efeb1f73e6ffa8e828
Garo71426 <garo1997iskandaryan@gmail.com>
2020-02-06 16:29:42 +0400

    add screenshot

---
+++ build/precache-manifest.036d73da924e877031afd0208c7c653b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12372ae5d4f41a690905ca5018c90d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f09ab0de096a94a2a8",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0ed076328c2bbee5e37b",
+    "url": "/static/css/main.cf924a2c.chunk.css"
+  },
+  {
+    "revision": "d7f09ab0de096a94a2a8",
+    "url": "/static/js/2.64f64a25.chunk.js"
+  },
+  {
+    "revision": "0ed076328c2bbee5e37b",
+    "url": "/static/js/main.94c89087.chunk.js"
+  },
+  {
+    "revision": "b3875654d4e7ef0270f6",
+    "url": "/static/js/runtime~main.06ecdbf6.js"
+  }
+]);
